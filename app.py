a66780cb31645066415942895d550fc5092d2140
koensantos <95321766+koensantos@users.noreply.github.com>
2026-08-02 21:49:07 -0400

    Implemented the Kaggle Spotify Dataset into a PostgresSQL Datbaase.

--- app.py
+++ app.py
@@ -6,6 +6,23 @@ df = pd.read_csv('dataset/dataset.csv')
 print(df.head())  # Display the first few rows of the DataFrame
 print(df.columns.tolist())  # Display the column names of the DataFrame
 
+def clean_dataframe(df):
+    #Cleans dataframe by removing dups, handles missing values
+    df = df.drop_duplicates()  # Remove duplicate rows
+    df = df.dropna()  # Remove rows with missing values
+    if df.duplicated().any():
+        print("Warning: There are still duplicate rows in the DataFrame.")
+    if df.isna().sum().any():
+        print("Warning: There are still missing values in the DataFrame.")
+    return df
+df = clean_dataframe(df)
+
 engine = create_engine('sqlite:///song_dataset.db')
 
 df.to_sql('songs', con=engine, if_exists='replace', index=False)
+
+check_df = pd.read_sql('SELECT * FROM songs', con=engine)
+print(check_df.head())  # Display the first few rows of the DataFrame read from the
+
+get_track_name = pd.read_sql('SELECT track_name FROM songs', con=engine)
+print(get_track_name.head()) 
---
+++ backend/song_retrieval.py
@@ -0,0 +1,13 @@
+#Retrieves songs from the database based on the provided track and artist name.
+#If not in current database, use reccobeats.com API to search for song.
+#If found, add to database and return song information.
+#Otherwise, return None. Let user in frontend know that song was not found.
+
+def get_song_from_database(track_name, artist_name, engine):
+
+
+def get_song_from_api
+
+def check_song_in_database
+
+def add_song_to_database
