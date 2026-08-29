b1cb00bb0ce8608a093f4f0475368709699d2fd2
koensantos <95321766+koensantos@users.noreply.github.com>
2026-08-28 23:54:18 -0400

    Added React Frontend and tests for App.js

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,10 @@ import math
 
 from similarity_score import calculate_similarity_score
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 RECOMMENDATION_LIMIT = 5
 DEFAULT_GENRE = "pop"
